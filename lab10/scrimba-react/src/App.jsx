62fa2670b4f2180bf31e3b84bc6c0e3f82c937b9
Satalicious <color.ui>
2022-08-01 15:24:26 +0200

    added lab

---
+++ lab10/scrimba-react/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+//import Web3 from 'web3'
+
+function App() {
+
+  return (
+    <div className="App">
+      <img className="pic" src="../images/pic.png"/>
+      <h1>Front & Backend Developer</h1>
+    </div>
+  )
+}
+
+export default App
